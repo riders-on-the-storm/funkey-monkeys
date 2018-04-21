4388fe8496b029f3bfdc2a48ebba6c52c441ab70
Valchuk Dmitry <dart.fk712@gmail.com>
2018-04-21 09:36:42 +0600

    add crap gameobject

---
+++ Assets/Scripts/CrapScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrapScript : MonoBehaviour
+{
+
+	public Vector3  StartForce = new Vector3(5, 2);
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = this.GetComponent<Rigidbody2D>();
+		rb.AddForce(StartForce);
+	}
+
+	private void FixedUpdate()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		
+	}
+}
