a98078c1cc3c66911702102dcba0080a1a837386
Sergey Pavlov <svkpavlov@gmail.com>
2018-04-21 23:33:48 +0600

    power of throw has been added

--- Assets/Scripts/MonkeyKeyController.cs
+++ Assets/Scripts/MonkeyKeyController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class MonkeyKeyController : MonoBehaviour {
 	private Animator animator;
-
+	private float Force;
+	private float MaxForce = 350;
+	private float TimeStart;
+	private float DeltaTime = 0;
+	private float DeltaTimeMax = 1;
+	private bool Accumulation = false;
 	public GameObject crap;
 	// Use this for initialization
 	void Start () {
@@ -12,14 +17,52 @@ public class MonkeyKeyController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
 	{
-		if (Input.GetKeyDown("space"))
+		if (Input.GetMouseButton(0))
 		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				TimeStart = Time.time;
+				Accumulation = true;
+			}
+
+			if (Accumulation)
+			{
+				DeltaTime = Time.time - TimeStart;
+				if (DeltaTime > DeltaTimeMax)
+				{
+					DeltaTime = DeltaTimeMax;
+				}
+			}
+		}
+
+		if ((Accumulation && !Input.GetMouseButton(0)) || (DeltaTime == DeltaTimeMax))
+		{
+			Force = (DeltaTime / DeltaTimeMax) * MaxForce;
+			Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pz.z = 0;
+			Vector2 forceVector = (pz - this.transform.position);
+			forceVector = pz / pz.magnitude;
+			forceVector = forceVector * Force;
+			
 			animator.SetTrigger("KeyClick");
 			GameObject go = Instantiate(crap) as GameObject;
-			Vector2 position = this.transform.position;
-			go.transform.position = position;
-		}			
+			go.transform.position = this.transform.position;
+			go.GetComponent<CrapScript>().StartForce = forceVector;
+			
+			Accumulation = false;
+			DeltaTime = 0;
+		}
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("SomeLevel");
 	}
 }
