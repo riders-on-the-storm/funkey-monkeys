86debfd9015bb60a9a4a9d01cf9b15ffe223f51e
Valchuk Dmitry <dart.fk712@gmail.com>
2018-04-21 13:04:50 +0600

    add game object with generation of other objects

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+
+	public Camera Camera;
+	public GameObject Player1;
+	public GameObject Player2;
+	public Grid MainGrid;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate(Camera);
+		Instantiate(Player1);
+		if (Player2 != null)
+		{
+			Instantiate(Player2);
+		}
+		Instantiate(MainGrid);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
