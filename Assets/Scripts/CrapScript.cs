ff0f4330d9b28bdcdaaba02c974d2ea879716c97
Sergey Pavlov <svkpavlov@gmail.com>
2018-04-22 15:46:45 +0600

    destroy the crap has been added

--- Assets/Scripts/CrapScript.cs
+++ Assets/Scripts/CrapScript.cs
@@ -7,16 +7,13 @@ public class CrapScript : MonoBehaviour
 
 	public Vector3  StartForce;
 	private Rigidbody2D rb;
+	private float TimeStart;
 	// Use this for initialization
 	void Start ()
 	{
 		rb = this.GetComponent<Rigidbody2D>();
 		rb.AddForce(StartForce);
-	}
-
-	private void FixedUpdate()
-	{
-		
+		Destroy(gameObject, 2);
 	}
 
 	// Update is called once per frame
